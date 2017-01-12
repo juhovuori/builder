b2f97d11deb27319fac240749e15325566634673
Juho Vuori <juhovuo@gmail.com>
2017-01-12 18:26:48 +0200

    More realistic build script for builder

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-echo moe
-echo fail >&2
-
+export GOPATH=$(pwd)
+go get github.com/juhovuori/builder
+cp bin/builder ~
+cp src/github.com/juhovuori/builder/builder.hcl ~
+cp src/github.com/juhovuori/builder/project.hcl ~
+killall builder # systemd should restart
