16420560dbb2689eaf156be59ae3467a63822478
Juho Vuori <juhovuo@gmail.com>
2017-01-12 21:10:50 +0200

    Fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 export GOPATH=$(pwd)
-go get github.com/juhovuori/builder
-cp bin/builder ~
-cp src/github.com/juhovuori/builder/builder.hcl ~
-cp src/github.com/juhovuori/builder/project.hcl ~
+go get -d github.com/juhovuori/builder
+cd src/github.com/juhovuori/builder
+make build
+mv bin/builder ~
+cp builder.hcl ~
+cp project.hcl ~
+cp version.json ~
 killall builder # systemd should restart
