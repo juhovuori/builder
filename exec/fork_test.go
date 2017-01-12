ee7ccf2115cb7e86bbe0be0ed070bbd70e0cb622
Juho Vuori <juhovuo@gmail.com>
2017-01-12 08:20:57 +0200

    Test for script failure

--- exec/fork_test.go
+++ exec/fork_test.go
@@ -6,32 +6,40 @@ import (
 )
 
 func TestForkExecutor(t *testing.T) {
-	dir := tmpFilename()
-	script := "testdata/success.sh"
-	e := forkExecutor{dir, script}
-	ch, err := e.Run()
-	if err != nil {
-		t.Fatalf("Run returned error: %v\n", err)
+	cases := []struct {
+		script string
+		status int
+	}{
+		{"testdata/success.sh", 0},
+		{"testdata/fail.sh", 1},
 	}
-	info, err := os.Stat(dir)
-	if err != nil {
-		t.Fatalf("Error in stat %s: %v\n", dir, err)
-	}
-	if !info.IsDir() {
-		t.Errorf("%s is not a directory.\n", dir)
-	}
+	for _, c := range cases {
+		dir := tmpFilename()
+		e := forkExecutor{dir, c.script}
+		ch, err := e.Run()
+		if err != nil {
+			t.Fatalf("Run returned error: %v\n", err)
+		}
+		info, err := os.Stat(dir)
+		if err != nil {
+			t.Fatalf("Error in stat %s: %v\n", dir, err)
+		}
+		if !info.IsDir() {
+			t.Errorf("%s is not a directory.\n", dir)
+		}
 
-	status := <-ch
-	if status != 0 {
-		t.Errorf("Got wrong exit status %d.\n", status)
-	}
+		status := <-ch
+		if status != c.status {
+			t.Errorf("Got wrong exit status %d, expected %d.\n", status, c.status)
+		}
 
-	if err = e.Cleanup(); err != nil {
-		t.Errorf("Cleanup returned error %v.\n", err)
-	}
+		if err = e.Cleanup(); err != nil {
+			t.Errorf("Cleanup returned error %v.\n", err)
+		}
 
-	info, err = os.Stat(dir)
-	if err == nil {
-		t.Errorf("Expected error %+v\n", info)
+		info, err = os.Stat(dir)
+		if err == nil {
+			t.Errorf("Expected error %+v\n", info)
+		}
 	}
 }
