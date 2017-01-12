aa4e2eb8f8c39b1c17ea9ab51016f99294febd23
Juho Vuori <juhovuo@gmail.com>
2017-01-12 19:10:19 +0200

    New style deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,16 +2,19 @@
 
 FILES="builder builder.hcl project.hcl version.json"
 
-echo Stopping builder
-killall builder
+
+mkdir -p deploy
 
 echo Copying files
 for FILE in $FILES
 do
   echo $FILE
-  curl -o "$FILE" "https://s3.eu-central-1.amazonaws.com/juhovuori/builder/$FILE"
+  curl -o "deploy/$FILE" "https://s3.eu-central-1.amazonaws.com/juhovuori/builder/$FILE"
 done
 
-echo Restarting
-nohup ./builder server > builder.out 2> builder.err < /dev/null &
-echo Done
+mv deploy/* .
+rmdir deploy
+
+echo Restart builder
+killall builder
+
