f363874c9c74a86d388aebefde5769fbabced7c5
Juho Vuori <juhovuo@gmail.com>
2017-01-20 22:07:45 +0200

    Removed stale code from repository

--- repository/id.go
+++ repository/id.go
@@ -2,17 +2,6 @@ package repository
 
 import uuid "github.com/satori/go.uuid"
 
-// Config is a repository configuration
-type Config struct {
-	Type Type
-	URL  string
-}
-
-// ID returns an ID for a repository
-func (c Config) ID() string {
-	return c.ID()
-}
-
 // ID returns an ID for a repository
 func ID(t Type, URL string) string {
 	return uuid.NewV5(namespace, string(t)+":"+URL).String()
